726edb09b56ab85d63868f77400ee26ce6befa03
Halley <74821658+KometHalley@users.noreply.github.com>
2020-12-08 21:41:08 +0100

    [B] add noscript warning and link to metamask on missing wallets (#2)
    * [B] add noscript warning and link to metamask on missing wallets
    
    * fix the domain name reset

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { setup } from './ts/setup'
 import './ts/custom-elements/plasma'
 
 // eslint-disable-next-line no-undef
-ethereum.autoRefreshOnNetworkChange = false
-
-setup()
+console.log('ethereum ? ', window.ethereum, !!window.ethereum)
+if (window.ethereum) {
+  // eslint-disable-next-line no-undef
+  window.ethereum.autoRefreshOnNetworkChange = false
+  setup()
+} else {
+  document.getElementById('install-metamask').hidden = false
+}
