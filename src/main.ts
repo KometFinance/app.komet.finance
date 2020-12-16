0196d5f8acbd4b7ee7137952559c0961c3dd886f
Halley <74821658+KometHalley@users.noreply.github.com>
2020-12-16 16:40:28 +0100

    [F] prepare for launch (#31)
    * [F] prepare for launch
    
    * point to app.komet.finance
    
    * fix linting

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import './tailwind-build.css'
 import './main.scss'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'babel-polyfill'
-import { setup } from './ts/setup'
+// import { setup } from './ts/setup'
 import './ts/custom-elements/plasma'
 
 if (window.ethereum) {
   // eslint-disable-next-line no-undef
   (window.ethereum as any).autoRefreshOnNetworkChange = false
-  setup()
+  // setup()
 } else {
   document.getElementById('install-metamask').hidden = false
 }
